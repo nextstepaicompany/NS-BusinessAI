6f01945ee124146f5f81d861a44f95dc3bf1f309
nextstepaicompany <u2291761274@gmail.com>
2025-12-24 11:48:28 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
+function entrarCola() {
+  document.getElementById("formulario").style.display = "none";
+  document.getElementById("cola").style.display = "block";
+
+  const personas = Math.floor(Math.random() * 5) + 1;
+  document.getElementById("mensajeCola").innerText =
+    "Hay " + personas + " personas delante de ti";
+
+  let tiempo = 60;
+  const intervalo = setInterval(() => {
+    tiempo--;
+    document.getElementById("tiempo").innerText = tiempo;
+
+    if (tiempo <= 0) {
+      clearInterval(intervalo);
+      document.getElementById("mensajeCola").innerText =
+        "Generando contenido con IA...";
+    }
+  }, 1000);
+}
 
